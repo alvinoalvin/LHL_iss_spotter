a7c40735c5056b484171bdf6f6313f05bb0e6471
Alvin Ng <alvinoalvin123@gmail.com>
2021-04-09 00:25:38 +0000

    removed comments

--- index.js
+++ index.js
@@ -1,30 +1,5 @@
 // index.js
-const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes, nextISSTimesForMyLocation } = require('./iss');
-
-// fetchMyIP((error, ip) => {
-//   if (error) {
-//     console.log("It didn't work!", error);
-//     return;
-//   }
-//   console.log('It worked! Returned IP:', ip);
-
-//   fetchCoordsByIP(ip, (error, data) => {
-//     if (error) {
-//       console.log("It didn't work!", error);
-//       return;
-//     }
-//     console.log('It worked! Returned Latitude:', data.latitude, ' Longitude', data.longitude);
-
-//     fetchISSFlyOverTimes({ latitude: data.latitude, longitude: data.longitude }, (error, data) => {
-//       if (error) {
-//         console.log("It didn't work!", error);
-//         return;
-//       }
-//       console.log(data);
-//     });
-//   }
-//   );
-// });
+const { nextISSTimesForMyLocation } = require('./iss');
 
 nextISSTimesForMyLocation((error, data) => {
   if (error) {
